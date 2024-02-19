595d6754e0e734a54b3e0e890afd775c7507d8a2
manuelloverso <manuelloverso@gmail.com>
2024-02-19 15:29:56 +0100

    add discounts

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 // Variables declaration
+let price;
 const pricePerKm = 0.21;
 const numberKm = Number(
   prompt("Inserisci il numero di km che vuoi percorrere")
@@ -7,3 +8,18 @@ console.log(numberKm, typeof numberKm);
 
 const age = Number(prompt("Inserisci la tua età"));
 console.log(age, typeof age);
+
+// Calculate the price based on price per km
+price = numberKm * pricePerKm;
+console.log(price + "€");
+
+//apply the discount
+if (age < 18) {
+  price = (price * 80) / 100;
+  console.log(price);
+}
+
+if (age > 65) {
+  price = (price * 60) / 100;
+  console.log(price);
+}
