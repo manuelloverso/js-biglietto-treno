5ffead714dc3bce1acdb8cb966bc55ff68f3b7fe
manuelloverso <manuelloverso@gmail.com>
2024-02-19 15:14:02 +0100

    declare variables

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+// Variables declaration
+const pricePerKm = 0.21;
+const numberKm = Number(
+  prompt("Inserisci il numero di km che vuoi percorrere")
+);
+console.log(numberKm, typeof numberKm);
+
+const age = Number(prompt("Inserisci la tua età"));
+console.log(age, typeof age);
