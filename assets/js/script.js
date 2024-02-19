fa211b3df3c343f88e6baf79fcb3652cea4b74d7
manuelloverso <manuelloverso@gmail.com>
2024-02-19 15:37:38 +0100

    add print when discount doesnt apply

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 // Variables declaration
 let price;
+let sconto = false;
 const pricePerKm = 0.21;
 const numberKm = Number(
   prompt("Inserisci il numero di km che vuoi percorrere")
@@ -11,15 +12,23 @@ console.log(age, typeof age);
 
 // Calculate the price based on price per km
 price = numberKm * pricePerKm;
-console.log(price + "€");
+console.log("Il prezzo del biglietto non scontato è " + price + "€");
 
 //apply the discount
 if (age < 18) {
   price = (price * 80) / 100;
-  console.log(price);
+  sconto = true;
 }
 
 if (age > 65) {
   price = (price * 60) / 100;
-  console.log(price);
+  sconto = true;
 }
+
+// Print if discount doesn't apply at all
+if (sconto == false) {
+  console.log("Non hai diritto ad alcuno sconto");
+}
+
+// Print final price
+console.log("Il tuo prezzo finale è " + price + "€");
